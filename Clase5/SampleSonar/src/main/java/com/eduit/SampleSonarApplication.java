855f7c6f3fbb3e007d45969fad113b5e972a3735
Rafael Ibarra <rafael.dante@gmail.com>
2021-01-07 22:03:57 -0300

    clase 5

---
+++ Clase5/SampleSonar/src/main/java/com/eduit/SampleSonarApplication.java
@@ -0,0 +1,13 @@
+package com.eduit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSonarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSonarApplication.class, args);
+	}
+
+}
